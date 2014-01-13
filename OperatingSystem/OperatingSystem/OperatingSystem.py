782e9e2bb115836c7726e93e2a5b109c37bb1ed2
saurabh7248 <saurabhc7248@hotmail.com>
2014-01-13 22:32:01 +0530

    $END for each job
    As suggested by my friend Abhisheik Patwardhan.

--- OperatingSystem/OperatingSystem/OperatingSystem.py
+++ OperatingSystem/OperatingSystem/OperatingSystem.py
@@ -11,13 +11,16 @@ inputfile=open(r'E:\input.txt','r')
 output=open(r'E:\output.txt','w')
 C=False
 def MOS():
+    print('IN MOS')
     if(SI==1):
         READ()
     elif(SI==2):
         WRITE()
     elif(SI==3):
         TERMINATE()
+    print('back in MOS')
 def WRITE():
+    print('In write')
     global IR
     IR[3]='0'
     block=int(IR[2]+IR[3])
@@ -27,9 +30,12 @@ def WRITE():
             output.write(M[x][y])
     output.write('\n')
 def TERMINATE():
+    print('In terminate')
     output.write('\n\n')
     LOAD()
+    print('back in terminate')
 def READ():
+    print('In execute read')
     global M
     global IR
     block=int(IR[2]+IR[3])
@@ -47,10 +53,10 @@ def READ():
             M[block]=deepcopy(datacard[4*count:4*count+4])
             block+=1
 def EXECUTEUSERPROGRAM():
+    print('In execute user program')
     global IC
     global IR
     while(True):
-        print(IC)
         IC=int(IC[0]+IC[1])
         IR=deepcopy(M[IC])
         IC+=1
@@ -83,24 +89,29 @@ def EXECUTEUSERPROGRAM():
         elif(IR[0]=='H'):
             SI=3
             MOS()
+    print('bac in execute user program')
 def STARTEXECUTION():
+    print('In startexecution')
     global IC
     IC=['0','0']
     EXECUTEUSERPROGRAM()
+    print('bac in startexecution')
 def LOAD():
+    print('In Load')
     pointer=0
     inputfeed=inputfile.readline()
     while(inputfeed):
+        print(inputfeed)
         if(inputfeed.endswith('\n')):
             inputfeed=inputfeed[0:len(inputfeed)-1]
         if(inputfeed.startswith("$AMJ") or inputfeed.startswith("$DTA") or inputfeed.startswith("$END")):
-            '''if(inputfeed=='$AMJ'):
-                break'''
+            if(inputfeed=='$AMJ'):
+                pass
             if(inputfeed=='$DTA'):
                 STARTEXECUTION()
-            elif(inputfeed=='$END'):
+            '''elif(inputfeed=='$END'):
                 output.close()
-                exit(0)
+                exit(0)'''
         else:
             if(pointer==100):
                 exit(0)
@@ -112,5 +123,13 @@ def LOAD():
                     M[pointer]=deepcopy(inputfeed[4*count:4*count+4])
                     pointer+=1
         inputfeed=inputfile.readline()
+    if(not inputfeed):
+        output.close()
+        exit(0)
+    print('bac in load')
 LOAD()
+print('back in main')
 inputfile.close()
+output.close()
+exit(0)
+s
